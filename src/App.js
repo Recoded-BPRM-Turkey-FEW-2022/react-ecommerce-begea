dfb450ff9d8566a6ed24c82160c717055fbd7bfc
tezcan <tezcan@tezcans-MBP.home>
2022-06-20 16:18:49 +0300

    tailwindcss added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
 
 export default function App() {
   return (
     <div>
-      <h1>React Ecommerce!</h1>
+      <h1 className='text-7xl'>React Ecommerce!</h1>
+      <ul>
+        <li>asdasd</li>
+      </ul>
     </div>
   );
 }
