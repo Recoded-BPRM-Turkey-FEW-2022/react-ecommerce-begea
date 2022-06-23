d8dc9ec982075c2c04f1ec9fb0fd9edd0e2f8dcc
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-06-23 20:02:25 +0300

    cardView Edit

--- src/components/Product/CardView/CardView.jsx
+++ src/components/Product/CardView/CardView.jsx
@@ -1,6 +1,6 @@
 function CardView({ categories, title, price, image }) {
   return (
-    <div className="bg-white font-Roboto grid rounded-xl p-4 overflow-hidden shadow-md hover:shadow-xl w-64">
+    <div className="bg-white font-Roboto grid rounded-xl p-4 overflow-hidden shadow-md hover:shadow-xl w-64 transition-all duration-300">
       <div className="max-w-sm flex justify-center mb-4">
         <img
           src={image}
@@ -12,20 +12,6 @@ function CardView({ categories, title, price, image }) {
         <p className=" mb-2 block font-medium leading-5">{title}</p>
         <p className=" text-sm mb-1 font-extralight">{categories}</p>
         <p className=" text-md font-normal">$ {price}</p>
-      </div>
-      <div className="font-Roboto flex justify-between items-center">
-        <input
-          className="font-Roboto border rounded-md font-extralight text-sm p-1 m-1 w-20"
-          type="number"
-          id="quantity"
-          name="quantity"
-          min="0"
-          max="10"
-          placeholder="Quantity"
-        />
-        <button className="p-2">
-          <img className="flex m-2" src="assets/cart.png"></img>
-        </button>
       </div>
     </div>
   );
