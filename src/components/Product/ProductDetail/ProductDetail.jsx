a29638db46cb90bd73ac9d8b250aa9ec0ccad0d5
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-06-24 14:08:16 +0300

    single product

--- src/components/Product/ProductDetail/ProductDetail.jsx
+++ src/components/Product/ProductDetail/ProductDetail.jsx
@@ -23,32 +23,35 @@ const ProductDetail = ({ clickedProduct }) => {
 
   return (
     <div className="flex flex-wrap flex-row align-middle m-auto w-3/4 h-4/5 mt-8 mb-8 shadow-md justify-around ">
-
-        <a href="#">
-          <img src={clickedProduct.image} alt="fgd" />
-        </a>
-        <div className="flex-col justify-center">
-          {/* detayların düzeni yukarıdan aşağı olmalı*/}
-          <h1 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+      <a href="#">
+        <img
+          className="h-full  md-flex"
+          src={clickedProduct.image}
+          alt="./images/image.png"
+        />
+      </a>
+      <div className="flex flex-col flex-wrap  mt-8 p-10 ">
+        <div className="flex flex-wrap flex-col  text-center">
+          <p className="text-black font-bold text-3xl hover:underline">
             {clickedProduct.title}
-          </h1>
-          <p>{clickedProduct.description}</p>
-          <div className="">
-            <span className="text-3xl font-bold text-gray-900 dark:text-red-600">
-              {clickedProduct.price}
-            </span>
-          </div>
-
+          </p>
           <br></br>
-
-          <div className="text-center">
+          <p className="text-zinc-600 ">{clickedProduct.description}</p>
+          <br></br>
+          <br></br>
+          <span className="text-5xl font-bold dark:text-red-700">
+            ${clickedProduct.price}
+          </span>
+        </div>
+        <br></br>
+        <div className="text-center">
           <button className="text-white  hover:bg-red-800 focus:ring-4 hover:shadow-lg focus:outline-none focus:ring-red-300 font-medium text-2xl rounded-sm  px-5 py-2.5 w-60 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 mt-20">
             Add to Cart
           </button>
         </div>
       </div>
-      </div>
+    </div>
   );
-
+};
 
 export default ProductDetail;
