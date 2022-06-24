d6bc8aa56b33900557e3e8ade197fc652571a922
beyzabostanci <beyza_bostanc@hotmail.com>
2022-06-24 13:47:57 +0300

    product detail

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 import { ProductsContext } from './api/ProductsContext';
 import useProduct from './api/useProduct';
 
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
-import ListProducts from './components/Navbar/ListProducts/ListProducts';
 import Footer from './components/Footer/Footer';
-import Search from './components/Navbar/Search/Search';
 
 export default function App() {
-  const [foundProduct, setFoundProduct] = useState([]);
   const products = useProduct();
 
   return (
     <ProductsContext.Provider value={products}>
       <div className='bg-avokado font-opensans pb-5'>
-        <Navbar setFoundProduct={setFoundProduct} />
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/allproducts' element={<ListProducts />} />
-          <Route
-            path='/search-result'
-            element={<Search foundProduct={foundProduct} />}
-          />
-
-          {/* <Route path='/:product' element={<ProductDetail />} /> */}
-        </Routes>
+        <Navbar />
+        <Main />
       </div>
       <Footer />
     </ProductsContext.Provider>
--- src/components/Product/ProductDetail/ProductDetail.jsx
+++ src/components/Product/ProductDetail/ProductDetail.jsx
@@ -1,4 +1,7 @@
-const ProductDetail = (title, price, image, description) => {
+import { Link } from 'react-router-dom';
+
+
+const ProductDetail = (id, title, price, image, description) => {
 
   return (
     <div className="flex flex-wrap flex-row align-middle m-auto w-3/4 h-4/5 mt-8 mb-8 shadow-md justify-around ">
@@ -30,6 +33,7 @@ const ProductDetail = (title, price, image, description) => {
           <button className="text-white  hover:bg-red-800 focus:ring-4 hover:shadow-lg focus:outline-none focus:ring-red-300 font-medium text-2xl rounded-sm  px-5 py-2.5 w-60 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 mt-20">
             Add to Cart
           </button>
+
           </div>
 
         </div>
