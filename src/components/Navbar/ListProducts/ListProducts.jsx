5c6f6ff2151d6291e1c02e5c72627a739a52bbca
tezcan <tezcan@tezcans-MBP.home>
2022-06-24 09:50:57 +0300

    search box

--- src/components/Navbar/ListProducts/ListProducts.jsx
+++ src/components/Navbar/ListProducts/ListProducts.jsx
@@ -1,23 +1,24 @@
+import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { ProductsContext } from '../../../api/ProductsContext';
 import CardView from '../../Product/CardView/CardView';
 
 export default function ListProducts() {
   const products = useContext(ProductsContext);
-  // console.log(products);
+
   return (
-    <>
+    <div className='flex flex-wrap gap-8 w-3/4 mt-8 max-w-7xl m-auto justify-around'>
       {products.map((product, index) => {
         return (
           <CardView
             key={index}
             title={product.title}
-            // categories={product.category.name}
+            categories={product.category.name}
             price={product.price}
-            // image={product.images[0]}
+            image={product.images[0]}
           />
         );
       })}
-    </>
+    </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,50 @@
 // this will be a navbar that contain, logo, search, categories, all products,
 // login, signup and cart
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { GrCart } from 'react-icons/gr';
 import { RiUser3Line } from 'react-icons/ri';
 import { FiUserPlus } from 'react-icons/fi';
+import { BsSearch } from 'react-icons/bs';
+import useProduct from '../../api/useProduct';
 
-export default function Navbar() {
+export default function Navbar({ setFoundProduct }) {
+  const [searchInput, setSearchInput] = useState('');
+  const product = useProduct();
+
+  useEffect(() => {
+    const findSearchedProduct = (search) => {
+      const fixSearch = search.charAt(0).toUpperCase() + search.slice(1);
+      let filteredProducts = product.filter((item) =>
+        item.title.includes(fixSearch),
+      );
+      setFoundProduct(filteredProducts);
+    };
+    findSearchedProduct(searchInput.toLowerCase());
+  }, [searchInput]);
+
   return (
     <div className=' bg-navbar drop-shadow-[0_8px_24px_rgba(149,157,165,0.2)] mb-[5rem]'>
       <nav className='flex justify-between items-center h-[7.5vh] max-w-7xl m-auto'>
         <div className='logo text-2xl pl-5'>
-          <h1 className='font-bold '>Shopmop</h1>
+          <h1 className='font-bold '>
+            <Link to='/'>Shopmop</Link>
+          </h1>
         </div>
         <div className='nav-links pr-5'>
           <ul className='flex items-center gap-10 text-lg'>
+            <li className='hover:underline flex justify-center items-center gap-2'>
+              <input
+                type='text'
+                placeholder='Search for product'
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                className='bg-transparent border-y border-x p-1 pl-2'
+              />
+              <Link to='/search-result'>
+                <BsSearch />
+              </Link>
+            </li>
             <li className='hover:underline'>
               <a href='#' className='flex justify-center items-center gap-2'>
                 {' '}
@@ -20,7 +52,7 @@ export default function Navbar() {
               </a>
             </li>
             <li className='hover:underline'>
-              <a href='#'>All Products</a>
+              <Link to='/allproducts'>All Products</Link>
             </li>
             <li className='hover:underline'>
               <a href='#' className='flex justify-center items-center gap-2'>
